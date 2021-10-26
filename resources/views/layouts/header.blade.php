d471974accb84eda784f85f4050c2421f16d6451
Fikri Zufri <fikri@student.spb.ac.id>
2021-10-26 11:19:34 +0800

    remove index.html

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,7 +4,7 @@
             <div class="logo-area">
                 <div class="row align-items-center">
                     <div class="logo col-lg-3 text-center text-lg-left mb-3 mb-md-5 mb-lg-0">
-                        <a class="d-block" href="index.html">
+                        <a class="d-block" href="{{route('home')}}">
                             <img loading="lazy" src="images/logo.png" alt="KSS Group">
                         </a>
                     </div><!-- logo end -->
